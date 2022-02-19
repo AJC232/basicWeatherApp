971bbff715bb566369f50c2f3cbd2d554a3dad37
AJC232 <70858054+AJC232@users.noreply.github.com>
2022-02-19 18:50:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-const port = 5000;
-
-app.listen(process.env.PORT || port, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server is listening at https://localhost:${port}");
 });
 
